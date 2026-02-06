961b03985295e4f06b2c4c04f365bd61a5ea84ad
Yuyuto <yukkurigame0101@gmail.com>
2026-02-06 14:37:42 +0900

    ModRegistries.java を更新

--- src/main/java/com/yourname/yourmod/api/libs/ModRegistries.java
+++ src/main/java/com/yourname/yourmod/api/libs/ModRegistries.java
@@ -1,33 +1,24 @@
 package com.yourname.yourmod.api.libs;
 
 import com.yourname.yourmod.loader.Platform;
-import java.util.function.Supplier;
 
 public final class ModRegistries {
 
     private ModRegistries() {}
 
-    public static <T> T registerItem(String name, Supplier<T> supplier) {
-        T item = supplier.get();
-        Platform.get().registerItem(name, item);
-        return item;
+    public static void registerItem(String id, Object item) {
+        Platform.get().registries().item(id, item);
     }
 
-    public static <T> T registerBlock(String name, Supplier<T> supplier) {
-        T block = supplier.get();
-        Platform.get().registerBlock(name, block);
-        return block;
+    public static void registerBlock(String id, Object block) {
+        Platform.get().registries().block(id, block);
     }
 
-    public static <T> T registerEntity(String name, Supplier<T> supplier) {
-        T entity = supplier.get();
-        Platform.get().registerEntity(name, entity);
-        return entity;
+    public static void registerEntity(String id, Object entityType) {
+        Platform.get().registries().entity(id, entityType);
     }
 
-    public static <T> T registerBlockEntity(String name, Supplier<T> supplier) {
-        T be = supplier.get();
-        Platform.get().registerBlockEntity(name, be);
-        return be;
+    public static void registerBlockEntity(String id, Object blockEntityType) {
+        Platform.get().registries().blockEntity(id, blockEntityType);
     }
 }
