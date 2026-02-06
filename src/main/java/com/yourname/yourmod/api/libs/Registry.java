e92ab45b7f690b1b685a4663f1b8c36bb4cf609e
Yuyuto <yukkurigame0101@gmail.com>
2026-02-06 14:35:59 +0900

    Registry.java を更新

--- src/main/java/com/yourname/yourmod/api/libs/Registry.java
+++ src/main/java/com/yourname/yourmod/api/libs/Registry.java
@@ -1,7 +1,8 @@
 package com.yourname.yourmod.api.libs;
 
-import com.yourname.yourmod.api.libs.internal.ItemBuilder;
-import com.yourname.yourmod.api.libs.internal.BlockBuilder;
+import com.yourname.yourmod.api.libs.internal.*;
+
+import net.minecraft.world.entity.Entity;
 
 public final class Registry {
 
@@ -13,5 +14,20 @@ public final class Registry {
 
     public static BlockBuilder block(String id) {
         return new BlockBuilder(id);
+    }
+
+    public static <T extends Entity> EntityBuilder<T> entity(
+            String id,
+            net.minecraft.world.entity.EntityType.EntityFactory<T> factory
+    ) {
+        return new EntityBuilder<>(id, factory);
+    }
+
+    public static <T extends net.minecraft.world.level.block.entity.BlockEntity>
+    BlockEntityBuilder<T> blockEntity(
+            String id,
+            net.minecraft.world.level.block.entity.BlockEntityType.BlockEntitySupplier<T> factory
+    ) {
+        return new BlockEntityBuilder<>(id, factory);
     }
 }
