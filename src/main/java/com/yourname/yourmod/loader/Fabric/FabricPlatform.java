85b1a25f9aca497f08c1528640e884330ba68f63
Yuyuto <yukkurigame0101@gmail.com>
2026-02-06 19:09:28 +0900

    FabricPlatform.java を更新

--- src/main/java/com/yourname/yourmod/loader/Fabric/FabricPlatform.java
+++ src/main/java/com/yourname/yourmod/loader/Fabric/FabricPlatform.java
@@ -1,17 +1,21 @@
 package com.yourname.yourmod.loader.fabric;
 
 import com.yourname.yourmod.loader.LoaderExpectPlatform;
+import com.yourname.yourmod.api.datagen.DataGen;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.resources.ResourceLocation;
 
 public final class FabricPlatform implements LoaderExpectPlatform {
 
+    private static final String MOD_ID = "yourmodid";
+
     private final Registries registries = new FabricRegistriesImpl();
     private final Network network = new FabricNetworkImpl();
     private final Events events = new FabricEventsImpl();
 
-    private static final String MOD_ID = "yourmodid";
+    // ★ DataGen
+    private final FabricDataGenImpl dataGen = new FabricDataGenImpl();
 
     @Override
     public ResourceLocation id(String path) {
@@ -36,5 +40,17 @@ public final class FabricPlatform implements LoaderExpectPlatform {
     @Override
     public Events events() {
         return events;
+    }
+
+    @Override
+    public DataGen dataGen() {
+        return dataGen;
+    }
+
+    /**
+     * Fabric DataGen Entrypoint から呼ばれる
+     */
+    public void setupDataGen(net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator gen) {
+        dataGen.setup(gen);
     }
 }
