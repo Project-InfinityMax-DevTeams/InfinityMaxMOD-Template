8ac98fdaec404d18411024df2ca60882bbacbac1
Yuyuto <yukkurigame0101@gmail.com>
2026-02-06 18:18:11 +0900

    Packet.java を作成

---
+++ src/main/java/com/yourname/yourmod/api/libs/packet/Packet.java
@@ -0,0 +1,52 @@
+package com.yourname.yourmod.api.libs.packet;
+
+import net.minecraft.network.FriendlyByteBuf;
+
+import java.util.function.Function;
+
+public final class Packet<T> {
+
+    private final String id;
+    private PacketDirection direction;
+    private Function<FriendlyByteBuf, T> decoder;
+    private SimplePacket.PacketEncoder<T> encoder;
+    private PacketHandler<T> handler;
+
+    private Packet(String id) {
+        this.id = id;
+    }
+
+    public static <T> Packet<T> define(String id) {
+        return new Packet<>(id);
+    }
+
+    public Packet<T> serverbound() {
+        this.direction = PacketDirection.C2S;
+        return this;
+    }
+
+    public Packet<T> clientbound() {
+        this.direction = PacketDirection.S2C;
+        return this;
+    }
+
+    public Packet<T> codec(
+            Function<FriendlyByteBuf, T> decoder,
+            SimplePacket.PacketEncoder<T> encoder
+    ) {
+        this.decoder = decoder;
+        this.encoder = encoder;
+        return this;
+    }
+
+    public Packet<T> handle(PacketHandler<T> handler) {
+        this.handler = handler;
+        return this;
+    }
+
+    public void register() {
+        PacketRegistry.register(new SimplePacket<>(
+                id, direction, decoder, encoder, handler
+        ));
+    }
+}
