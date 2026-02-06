18593a4c0bd794bdc326af45bc9828ba6ecff2db
Yuyuto <yukkurigame0101@gmail.com>
2026-02-06 18:19:15 +0900

    ModEvents.java を更新

--- src/main/java/com/yourname/yourmod/api/event/ModEvents.java
+++ src/main/java/com/yourname/yourmod/api/event/ModEvents.java
@@ -1,12 +1,3 @@
 package com.yourname.yourmod.api.event;
 
-import com.yourname.yourmod.loader.Platform;
-
-public final class ModEvents {
-
-    private ModEvents() {}
-
-    public static void init() {
-        Platform.get().registerEvents();
-    }
-}
+public interface ModEvent {}
