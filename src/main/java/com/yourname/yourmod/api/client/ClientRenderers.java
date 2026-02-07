40288f031e43345c9b25de2ab8800e7332a6b15a
Yuyuto <yukkurigame0101@gmail.com>
2026-02-07 10:37:27 +0900

    ClientRenderers.java を作成

---
+++ src/main/java/com/yourname/yourmod/api/client/ClientRenderers.java
@@ -0,0 +1,12 @@
+package com.yourname.yourmod.api.client;
+
+public final class ClientRenderers {
+
+    private ClientRenderers() {}
+
+    public static void register() {
+        // BlockEntityRenderer
+        // EntityRenderer
+        // RenderLayer
+    }
+}
