ea56857fb752e905f86cdabeb4a232c33706cfed
Yuyuto <yukkurigame0101@gmail.com>
2026-02-07 11:00:37 +0900

    LoaderExpectPlatform.java を更新

--- src/main/java/com/yourname/yourmod/loader/LoaderExpectPlatform.java
+++ src/main/java/com/yourname/yourmod/loader/LoaderExpectPlatform.java
@@ -5,13 +5,14 @@ import net.minecraft.resources.ResourceLocation;
 public interface LoaderExpectPlatform {
 
     ResourceLocation id(String path);
-
     boolean isClient();
 
     Registries registries();
     Network network();
     Events events();
-    Client client();
+    Client client(); // ★追加
+
+    /* ---------------- Registry ---------------- */
 
     interface Registries {
         void item(String name, Object item);
@@ -20,18 +21,29 @@ public interface LoaderExpectPlatform {
         void blockEntity(String name, Object blockEntityType);
     }
 
+    /* ---------------- Network ---------------- */
+
     interface Network {
         void register();
         void sendToServer(Object packet);
         void sendToPlayer(Object player, Object packet);
     }
 
+    /* ---------------- Events ---------------- */
+
     interface Events {
         void register();
         void onPlayerJoin(Object player);
     }
 
+    /* ---------------- Client ---------------- */
+
     interface Client {
         void init();
+
+        /** Server用 no-op */
+        static Client noop() {
+            return () -> {};
+        }
     }
 }
