983bbe123253ce9239960ecc4637a6c9d7807458
Yuyuto <yukkurigame0101@gmail.com>
2026-02-07 10:38:56 +0900

    ClientInputs.java を作成

---
+++ src/main/java/com/yourname/yourmod/api/client/ClientInputs.java
@@ -0,0 +1,11 @@
+package com.yourname.yourmod.api.client;
+
+public final class ClientInputs {
+
+    private ClientInputs() {}
+
+    public static void register() {
+        // KeyMapping 登録
+        // マウス入力
+    }
+}
