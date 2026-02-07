55767bd4a6afed9f3bbf501b103a29e3354ba8f1
Yuyuto <yukkurigame0101@gmail.com>
2026-02-07 10:37:03 +0900

    ClientEvents.java を作成

---
+++ src/main/java/com/yourname/yourmod/api/client/ClientEvents.java
@@ -0,0 +1,10 @@
+package com.yourname.yourmod.api.client;
+
+public final class ClientEvents {
+
+    private ClientEvents() {}
+
+    public static void register() {
+        // ClientTick / RenderTick / InputEvent をここに集約
+    }
+}
