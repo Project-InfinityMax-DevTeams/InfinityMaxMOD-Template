23a8cbdb096dc9c6e8c04fd2e805a0cd14d60eae
Yuyuto <yukkurigame0101@gmail.com>
2026-02-17 08:08:01 +0900

    StateKey.java を更新

--- src/main/java/com/yourname/yourmod/api/system/state/StateKey.java
+++ src/main/java/com/yourname/yourmod/api/system/state/StateKey.java
@@ -1,38 +1,22 @@
 package com.yourname.yourmod.api.system.state;
 
-import java.util.Objects;
-
 /**
- * Capability風の状態識別子。
+ * 型安全な状態識別子。
+ * インスタンス同一性で識別する。
  */
 public final class StateKey<T> {
 
     private final Class<T> type;
 
-    private StateKey(String id, Class<T> type) {
-        this.id = id;
+    private StateKey(Class<T> type) {
         this.type = type;
     }
 
-    public final class StateKey<T> {
-
-        private final Class<T> type;
-
-        private StateKey(Class<T> type) {
-            this.type = type;
-        }
-
-        public static <T> StateKey<T> create(Class<T> type) {
-            return new StateKey<>(type);
-        }
-
-        public Class<T> type() {
-            return type;
-        }
+    public static <T> StateKey<T> create(Class<T> type) {
+        return new StateKey<>(type);
     }
 
     public Class<T> type() {
         return type;
     }
-
 }
