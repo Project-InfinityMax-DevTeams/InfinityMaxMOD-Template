40d672a709ac799c91b608f66250c5d28d1f1e36
Yuyuto <yukkurigame0101@gmail.com>
2026-02-18 08:58:04 +0900

    もどしたぞい

--- src/main/java/com/yourname/yourmod/api/libs/ModRegistries.java
+++ src/main/java/com/yourname/yourmod/api/libs/ModRegistries.java
@@ -10,19 +10,19 @@ public final class ModRegistries {
         this.platformRegistries = platformRegistries;
     }
 
-    public <T> void registerItem(String id, T item) {
+    public static <T> void registerItem(String id, T item) {
         platformRegistries.item(id, item);
     }
 
-    public <T> void registerBlock(String id, T block, float strength, boolean noOcclusion) {
+    public static <T> void registerBlock(String id, T block, float strength, boolean noOcclusion) {
         platformRegistries.block(id, block);
     }
 
-    public <T, C> void registerEntity(String id, T entityType, C category, float width, float height) {
+    public static <T, C> void registerEntity(String id, T entityType, C category, float width, float height) {
         platformRegistries.entity(id, entityType, category, width, height);
     }
 
-    public <T, B> void registerBlockEntity(String id, T blockEntityType, B... blocks) {
+    public static <T, B> void registerBlockEntity(String id, T blockEntityType, B... blocks) {
         platformRegistries.blockEntity(id, blockEntityType, blocks);
     }
 }
