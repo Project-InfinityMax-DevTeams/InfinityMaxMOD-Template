5a71be81e231cfbe939c2b277f1dfa4fee927e1c
Yuyuto <yukkurigame0101@gmail.com>
2026-02-20 10:45:59 +0900

    もう一回

---
+++ forge/src/main/java/com/yuyuto/infinitymaxapi/InifnityMaxAPIForge.java
@@ -0,0 +1,11 @@
+package com.yuyuto.infinitymaxapi;
+
+import net.minecraftforge.fml.common.Mod;
+import com.yuyuto.infinitymaxapi.InfinityMaxAPI;
+
+@Mod("infinitymaxapi")
+public class InfinityMaxForge {
+    public InfinityMaxForge() {
+        InfinityMaxAPI.init();
+    }
+}
