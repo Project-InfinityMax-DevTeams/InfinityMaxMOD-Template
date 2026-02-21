9c06a6eeec3fd7b12c79ee8b7e3847afc3e7b50f
BuildTools <unconfigured@null.spigotmc.org>
2026-02-21 14:17:56 +0900

    実装

---
+++ src/main/java/com/yuyuto/infinitymaxapi/gamelibs/Energy.java
@@ -0,0 +1,30 @@
+package com.yuyuto.infinitymaxapi.gamelibs;
+
+public class Energy {
+    private double resistance; //抵抗値
+    private double voltage; //電圧
+    private double amphea; //電流
+    private double power; //電力
+    private double joule; //電力消費・発電量
+
+    public void update(double datetime){
+        //オームの法則 V=RI
+        voltage = resistance * amphea;
+        //電力 P=VI
+        power = voltage * amphea;
+        //発熱量 J=PS
+        joule += power * datetime;
+    }
+    public void setResistance(double resistance){
+        this.resistance = resistance;
+    }
+    public void setAmphea(double amphea){
+        this.amphea = amphea;
+    }
+    public double getVoltage(){
+        return voltage;
+    }
+    public double getJoule(){
+        return joule;
+    }
+}
